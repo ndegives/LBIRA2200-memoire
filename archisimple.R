fb25af28109be5c88b1d2c02c4620bf0c5f13d00
ndegives <nico.degives@gmail.com>
2023-05-25 11:43:21 +0200

    ,

--- archisimple.R
+++ archisimple.R
@@ -7,18 +7,22 @@ setwd("C:/Nico/Github/Memoire")
 source("ArchiSimple/src/io_archisimple.R")
 
 
-# Lecture du fichier de paramètre ----------------------------------------------
+# Les six variétés de Sorgho ---------------------------------------------------
+
+
+
+# Lecture du fichier de paramètre...............................................
 
 params <- read_archisimple_xml("base_parameter.xml")
 
-# Insérer les différents paramètres à modifier dans le batch -------------------
+# Insérer les différents paramètres à modifier dans le batch....................
 
-par <- data.frame(sim_length = rep(0,6),
-                  dmin=c(      0.1567  ,0.1567  ,0.1567   ,0.1567  ,0.1567  ,0.1297),
-                  RDM = c(     0.1874  ,0.2022  ,0.228    ,0.17545 ,0.1745  ,0.1611),
-                  CVDD = c(    0.3     ,0.3     ,0.3      ,0.3     ,0.3     ,0.3))
+par <- data.frame(
+  dmin=c(      0.1567  ,0.1567  ,0.1567   ,0.1567  ,0.1567  ,0.1297),
+  RDM = c(     0.1874  ,0.2022  ,0.228    ,0.17545 ,0.1745  ,0.1611)
+  )
 
-# for loop pour faire le tour de tous les paramètres ---------------------------
+# for loop pour faire le tour de tous les paramètres............................
 
 sims <- NULL
 setwd("ArchiSimple/src/archisimple93/")
@@ -51,7 +55,7 @@ for (variety in c('Amiggo','Biggben','Hyperion','Juno','Swingg','Vegga')) {
 setwd("../../../") 
 
 
-# Plot --------------------------------------------------------------------
+# Plot..........................................................................
 
 ## From the side
 for (variety in c('Amiggo','Biggben','Hyperion','Juno','Swingg','Vegga')) {
@@ -64,3 +68,38 @@ for (variety in c('Amiggo','Biggben','Hyperion','Juno','Swingg','Vegga')) {
       labs(title=variety)
     )
 }
+
+
+# Le maïs ----------------------------------------------------------------------
+
+params_maize <- data.frame(
+  ageAdv = 7,
+  CVDD = 0.3,
+  dAdv = 1,
+  distAdv = 20,
+  dmax = ,
+  dmin = 0.14,
+  dSem = 1,
+  EL =  51,
+  erAdv = 0.8,
+  erSem =  0.5,
+  exportName = 'myroot' ,
+  exportType = 1,
+  GDs = 50,
+  IPD =  2,
+  LDC =  3000,
+  maxAdv = 40,
+  maxSem =  5,
+  pdmax =  0.8,
+  pdmin =  0,
+  PDT = 4.5,
+  RDM =  0.12,
+  SGC =  0,
+  sim_length = 20,
+  simtime =  140,
+  TMD =  0.08,
+  TrInt = 0.01,
+  TrT = 1 
+)
+
+
